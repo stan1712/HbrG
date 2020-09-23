b7a849b60f36788fa70e82669072cb06a75ab30d
stanislasbdx <perso@stan1712.com>
2020-09-23 15:18:41 +0200

    ldan page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 style="margin-top: 20%;">Bientôt ici, un nouveau site tout beau d'hébergement de fichier</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data: () => ({
+    test: process.env.VUE_APP_TITLE
+  })
 }
 </script>
