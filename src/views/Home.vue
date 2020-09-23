778c09512737253e69ff514b7d5f75260515f790
stanislasbdx <perso@stan1712.com>
2020-09-23 15:34:01 +0200

    better landing page 🙃

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1 style="margin-top: 20%;">Bientôt ici, un nouveau site tout beau d'hébergement de fichier</h1>
+    <h3>Dans le meilleur des cas, je vais essayer de faire un host avec des comptes utilisateurs comme ça vous retrouverez vos fichiers.</h3>
   </div>
 </template>
 
